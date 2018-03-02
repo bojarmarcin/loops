4cafb454bb22f8a405e02438237e850203d0af47
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-02 19:19:18 +0100

    Fix script at scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function drawtree(amount) {
+  var amount = prompt('How many rows do you want in your christmas tree?');
   for(var i = 0; i < amount; i++ ) {
-    var star = ' ';
 
     for(var j = 0; j < i; j++ ) {
-      star += '*';
+      document.write('*');
     }
-    document.write(star);
+    document.write('<br>');
   }
 }
-drawtree(40);
+drawtree();
